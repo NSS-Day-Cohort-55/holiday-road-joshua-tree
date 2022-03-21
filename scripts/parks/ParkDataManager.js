64978d0e898139e0f3956f26f2e2177e10587236
BenWatts9 <brandolphwatts@gmail.com>
2022-03-21 14:18:27 -0500

    push for nathan

--- scripts/parks/ParkDataManager.js
+++ scripts/parks/ParkDataManager.js
@@ -2,7 +2,7 @@
 
 //Used to retrieve National Parks data from API
 export const getParks = () => {
-    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=pqyN4FMCqAwMm6pCPoWL6YcGuf7rfI6ecDeVrSku`)
+    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=pqyN4FMCqAwMm6pCPoWL6YcGuf7rfI6ecDeVrSku&limit=465`)
     .then(response => response.json())
 }
 
