97e9c51397854af8babf447728d65698886186d9
BenWatts9 <brandolphwatts@gmail.com>
2022-03-21 16:05:26 -0500

    keyyyyys

--- scripts/parks/ParkDataManager.js
+++ scripts/parks/ParkDataManager.js
@@ -1,8 +1,9 @@
+import { settings } from "../Settings.js"
 
 
 //Used to retrieve National Parks data from API
 export const getParks = () => {
-    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=pqyN4FMCqAwMm6pCPoWL6YcGuf7rfI6ecDeVrSku&limit=465`)
+    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=${setting.npsKey}&limit=465`)
     .then(response => response.json())
 }
 
